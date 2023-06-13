5d18547239706fed926cd2ab027f54d237e7845c
Rodrigo Otazu <otazu.quispe.rodrigo@gmail.com>
2023-06-13 14:38:36 -0500

    commit inicial

---
+++ src/main/java/com/fantasy/market/FantasyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.fantasy.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fantasy/market/FantasyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasy.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
